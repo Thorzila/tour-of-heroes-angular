1b3408771bb8cf1e73536f560094915bd0e07777
Juan Manzano <juancamilo106055@gmail.com>
2021-01-16 18:13:49 -0500

    Initial version

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../models/hero';
+import { HEROES } from '../db/mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.scss']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
