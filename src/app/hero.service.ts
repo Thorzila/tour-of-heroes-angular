1bb54cdc5e771f260f555c70829052053ea2a3bf
Juan Manzano <juancamilo106055@gmail.com>
2021-02-04 17:07:26 -0500

    Update hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -14,6 +14,9 @@ import { HtmlParser } from '@angular/compiler';
 export class HeroService {
 
   private heroesUrl = 'api/heroes'; // URL web api
+  httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
 
   constructor(
     private messageService: MessageService,
@@ -60,6 +63,14 @@ export class HeroService {
       // Let the app keep running by returning an empty result.
       return of(result as T);
     }
+  }
+
+  /** PUT: update the hero on the server */
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
   }
 
 }
